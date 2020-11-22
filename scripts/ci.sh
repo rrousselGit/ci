06e3d54bbc7cd1d5247f8a0497a4f1a2d6f5da6b
Remi Rousselet <darky12s@gmail.com>
2020-11-22 15:29:00 +0000

    Import auto_test.sh

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,8 +1,14 @@
+# importing auto_test.sh
+mkdir scripts
+curl https://raw.githubusercontent.com/rrousselGit/ci/ci/scripts/auto_test.sh > auto_test.sh
+chmod +x scripts/auto_test.sh
+
+# installing dependencies
 dart pub global activate melos
 export PATH="$PATH":"$HOME/.pub-cache/bin"
-
 melos bootstrap
 
+# analyzing and testing the codebase
 melos exec -- dart format --set-exit-if-changed .
 melos exec -- dart analyze .
 melos exec --  ../../scripts/auto_test.sh
